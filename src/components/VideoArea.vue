4d449d9ee1c69c590ff07ef360e47a317e485c0b
Mazen Chami <mazenchami@Mazens-MacBook-Pro.local>
2020-02-07 15:18:14 -0500

    initial commit

---
+++ src/components/VideoArea.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div>
+          <vue-webrtc
+            ref="webrtc"
+            width="100%"
+            :roomId="roomId"
+            v-on:joined-room="logEvent"
+            v-on:left-room="logEvent"
+            v-on:open-room="logEvent"
+            v-on:share-started="logEvent"
+            v-on:share-stopped="logEvent"
+            @error="onError"
+          />
+        </div>
+        <div class="row">
+          <div class="col-md-12 my-3">
+            <button type="button" class="btn btn-primary" @click="onJoin">Join</button>
+            <button type="button" class="btn btn-primary" @click="onLeave">Leave</button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              v-if="isVideoPlaying"
+              @click="onCapture"
+            >Capture Photo</button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              v-if="isVideoPlaying"
+              @click="onShareScreen"
+            >Share Screen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-if="img">
+      <div class="col-md-12">
+        <h2>Captured Image</h2>
+        <figure class="figure">
+          <img :src="img" class="img-responsive" />
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoArea',
+  props: {
+    roomId: String,
+  },
+  data() {
+    return {
+      img: null,
+      isVideoPlaying: false,
+    };
+  },
+  computed: {
+    webRTCRef() {
+      return this.$refs.webrtc;
+    },
+  },
+  methods: {
+    onCapture() {
+      this.img = this.webRTCRef.capture();
+    },
+    onJoin() {
+      this.isVideoPlaying = true;
+      this.webRTCRef.join();
+    },
+    onLeave() {
+      this.isVideoPlaying = false;
+      this.webRTCRef.leave();
+    },
+    onShareScreen() {
+      this.img = this.webRTCRef.shareScreen();
+    },
+    onError(error, stream) {
+      console.log('On Error Event', error, stream);
+    },
+    logEvent(event) {
+      console.log('Event : ', event);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
+import WebRTC from 'vue-webrtc';
+import * as io from 'socket.io-client';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+window.io = io;
+
+Vue.use(WebRTC);
+
 Vue.config.productionTip = false;
 
+/* eslint arrow-parens: 0 */
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
